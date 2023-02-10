547a0d8fab6b9693747f06f191f15a06eef30a82
Hj. Malthaner <62077919+Varkalandar@users.noreply.github>
2023-02-10 17:07:56 +0100

    CHG: New position button for silver theme.

--- themes.src/silver/build.sh
+++ themes.src/silver/build.sh
@@ -1,6 +1,6 @@
 rm -rf *.pak
-../../src/makeobj/makeobj pak silver.pak standard.dat
-../../src/makeobj/makeobj pak silver-large.pak standard-large.dat
+../makeobj pak silver.pak standard.dat
+../makeobj pak silver-large.pak standard-large.dat
 mv *.pak ../../../../simutrans/themes
 cp *.tab ../../../../simutrans/themes
 
