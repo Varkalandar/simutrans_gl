9fd6e2c7f07390b8946abe0955a965170e192ad4
Hj. Malthaner <h.malthaner@gmx.de>
2023-12-17 14:23:48 +0100

    CHG: Window background now only 25% transparent, to make text easier to read.

--- src/simutrans/gui/gui_frame.cc
+++ src/simutrans/gui/gui_frame.cc
@@ -183,7 +183,7 @@ void gui_frame_t::draw(scr_coord pos, scr_size size)
 {
 	scr_size titlebar_size(0, ( has_title()*D_TITLEBAR_HEIGHT ));
 
-	display_set_color(rgba_t(1.0f, 1.0f, 1.0f, 0.5f));
+	display_set_color(rgba_t(1.0f, 1.0f, 1.0f, 0.75f));
 
 	// draw background
 	if(  opaque  ) {
