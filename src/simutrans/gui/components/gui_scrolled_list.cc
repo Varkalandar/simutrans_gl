fa848d260b67ce8b6e2735bd8e97901440d0fcf6
Hj. Malthaner <h.malthaner@gmx.de>
2023-12-31 15:22:15 +0100

    FIX: List background should now have even right border.

--- src/simutrans/gui/components/gui_scrolled_list.cc
+++ src/simutrans/gui/components/gui_scrolled_list.cc
@@ -36,7 +36,8 @@ void gui_scrolled_list_t::scrollitem_t::draw_background(const scr_coord offset) 
 	int odd = (pos.y / size.h) & 1;
 	rgba_t color = odd ? (gui_theme_t::gui_color_list_background_odd) : (gui_theme_t::gui_color_list_background_even);
 
-	display_fillbox_wh_clip_rgb(offset.x + pos.x - 4, offset.y + pos.y, size.w + 200, size.h, color, true);
+	// display_fillbox_wh_clip_rgb(offset.x + pos.x - 4, offset.y + pos.y, size.w + 200, size.h, color, true);
+	display_fillbox_wh_clip_rgb(offset.x + pos.x - 4, offset.y + pos.y, display_get_width(), size.h, color, true);
 }
 
 
