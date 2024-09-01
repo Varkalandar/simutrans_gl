4cd4d9c0942160b0f01cfcd1f792337e467fe675
Hj. Malthaner <h.malthaner@gmx.de>
2024-09-01 15:48:32 +0200

    FIX: tried to give tghe city infor window a proper size again.

--- src/simutrans/gui/city_info.cc
+++ src/simutrans/gui/city_info.cc
@@ -213,13 +213,14 @@ void city_info_t::init()
 	year_month_tabs.add_tab(&container_year, translator::translate("Years"));
 	year_month_tabs.add_tab(&container_month, translator::translate("Months"));
 	add_component(&year_month_tabs);
+	
 	// .. put the same buttons in both containers
 	button_t* buttons[MAX_CITY_HISTORY-1];
 	// add city charts
 
 	// year chart
 	container_year.set_table_layout(D_BUTTONS_PER_ROW,0);
-	container_year.set_force_equal_columns(true);
+	// container_year.set_force_equal_columns(true);
 	container_year.add_component(&chart, D_BUTTONS_PER_ROW);
 	chart.set_min_size(scr_size(0 ,8*LINESPACE));
 	chart.set_dimension(MAX_CITY_HISTORY_YEARS, 10000);
@@ -241,7 +242,7 @@ void city_info_t::init()
 
 	// month chart
 	container_month.set_table_layout(D_BUTTONS_PER_ROW, 0);
-	container_month.set_force_equal_columns(true);
+	// container_month.set_force_equal_columns(true);
 	container_month.add_component(&mchart, D_BUTTONS_PER_ROW);
 	mchart.set_pos(scr_coord(D_MARGIN_LEFT,1));
 	mchart.set_min_size(scr_size(0 ,8*LINESPACE));
@@ -249,7 +250,7 @@ void city_info_t::init()
 	mchart.set_seed(0);
 	mchart.set_background((SYSCOL_CHART_BACKGROUND));
 
-	container_month.add_table(4,3)->set_force_equal_columns(true);
+	container_month.add_table(4, 4)->set_force_equal_columns(true);
 	for(  uint32 i = 0;  i<MAX_CITY_HISTORY-1;  i++  ) {
 		sint16 curve = mchart.add_curve( color_idx_to_rgb(hist_type_color[i]), city->get_city_history_month(),
 			MAX_CITY_HISTORY, i, 12, gui_chart_t::STANDARD, (city->stadtinfo_options & (1<<i))!=0, true, 0 );
@@ -257,6 +258,9 @@ void city_info_t::init()
 		container_month.add_component(buttons[i]);
 		button_to_chart.append(buttons[i], &mchart, curve);
 	}
+
+	// chart.set_size({480, 200});
+	// mchart.set_size({480, 200});
 
 	update_labels();
 	set_resizemode(diagonal_resize);
